127b90b26a0b642f42630148ab9641897198c653
Farida <faridah.absi@gmail.com>
2018-06-25 09:13:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,8 +10,12 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  var price = Math.floor(Math.random() * 100);
+  cart.push({item: price});
+  console.log(`${item} has been added to your cart.`);
+  return cart;
 }
+
 
 function viewCart() {
   // write your code here
